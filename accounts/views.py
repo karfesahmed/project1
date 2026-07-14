2efbed632f3e766fce70c90035d2afb522c68490
karfesahmed <karfesahmed@gmail.com>
2026-07-14 18:49:53 +0100

    login page ,login & logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import redirect
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("home")
+        else:
+            return render(request,'dashboard/login.html',{
+                "message":"invalid username or password"
+            })
+    return render(request,'dashboard/login.html')
+def logout_view(request):
+    logout(request)
+    return redirect("home")
--- project1/settings.py
+++ project1/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project1.urls'
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
+
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
@@ -134,8 +136,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 STORAGES = {
-    "staticfiles" : {
-        "BACKEND" : "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
